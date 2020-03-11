b6caeaee60a03c002b208ef3465084b980cf3d25
Toan Ly <lydanhtoan8@gmail.com>
2020-03-10 21:14:55 -0700

    fix

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -1,62 +1,40 @@
-/** Intended for Arduino Uno R3
- * Boundless Squad
- */
+// Intended for Arduino Uno R3
+// Boundless Squad
 
-float seconds = 10 + 1;
+int seconds = 10;
 int pinNumber = 11;
 int pinNumber2 = 5;
 int pinNumber3 = 9;
 int pinNumber4 = 10;
-int state = 0;
 
 void setup() {
   // initialize digital pin LED_BUILTIN as an output.
-  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(pinNumber, OUTPUT);
+  pinMode(pinNumber2, OUTPUT);
+  pinMode(pinNumber3, OUTPUT);
+  pinMode(pinNumber4, OUTPUT);
   Serial.begin(9600);
   Serial.println("VIBRRRRRRRRRRRATING PILLOW");
 }
 
-// the loop function runs over and over again forever
-void loop() {
-  if (Serial.available() > 0) {
-    state = Serial.read();
-  }
-  // If second is 0, then the motors will stop
-  if (state == 0) {
-    if (minutesToSeconds())
-    {
-      analogWrite(pinNumber, 200);
-      analogWrite(pinNumber2, 200);
-      analogWrite(pinNumber3, 200);
-      analogWrite(pinNumber4, 200);
-      delay(1000);
-    }
-    else
-    {
-      Serial.println("Stop");
-      analogWrite(pinNumber, 0);
-      analogWrite(pinNumber2, 0);
-      analogWrite(pinNumber3, 0);
-      analogWrite(pinNumber4, 0);
-    }
-  } else {
-    Serial.println("Waiting for the green!");
-  }
+void runMotors(int freq){
+    analogWrite(pinNumber, freq);
+    analogWrite(pinNumber2, freq);
+    analogWrite(pinNumber3, freq);
+    analogWrite(pinNumber4, freq); 
 }
 
-/**
- * Convert from minutes to seconds and check if the time is still on
- * @return {boolean} - True if the time is still on
- */
-bool minutesToSeconds() {
-  if (seconds < 0) {
-    seconds = 0;
+// the loop function runs over and over again forever
+void loop() {
+  Serial.println(Serial.read());
+  runMotors(0);
+  // If there is data coming from the bluetooth module, then the motors will vibrate
+  while (Serial.available() > 0) {
+      Serial.println("Start");
+      Serial.flush(); //you might need to see if this is working,  
+                      //otherwise you can do a Serial.read() too
+      runMotors(200);
+      Serial.println("Stop");
+      runMotors(0);
   }
-  if (seconds == 0) {
-    return false;
-  }
-  seconds--;
-  Serial.println(seconds);
-  
-  return true;
 }
